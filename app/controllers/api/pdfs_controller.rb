d068e62a665253073ea0b8a5b9dae463085dd558
Adrian Mowat <adrian.mowat@gmail.com>
2020-12-31 17:07:55 +0000

    Added a very simple pdfs api

---
+++ app/controllers/api/pdfs_controller.rb
@@ -0,0 +1,16 @@
+class Api::PdfsController < ApplicationController
+  skip_before_action :verify_authenticity_token, only: [:update]
+
+  def update
+    pdf = Pdf.find(params[:id])
+    Pdf.update(pdf.id, url: pdf_params)
+    render json: {message: "OK"}
+  rescue ActiveRecord::RecordNotFound
+    render json: {message: "Not found"}, status: 404
+  end
+
+  private 
+  def pdf_params
+    params.require(:url)
+  end
+end

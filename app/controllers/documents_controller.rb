2cf0f989a6e5b5460344419fe26231a6cb2b1de7
Adrian Mowat <adrian.mowat@gmail.com>
2020-12-31 18:02:03 +0000

    Send pdf requests to sns

--- app/controllers/documents_controller.rb
+++ app/controllers/documents_controller.rb
@@ -1,4 +1,6 @@
 class DocumentsController < ApplicationController
+  after_action :request_pdf, only: [:create, :update]
+
   def index
     @documents = Document.all
   end
@@ -13,17 +15,20 @@ class DocumentsController < ApplicationController
 
   def create
     document = Document.new(document_params)
+    @document.pdfs.create
     if document.save
       redirect_to documents_path
     end
   end
-
+  
   def edit
     @document = Document.find(params[:id])
   end
-
+  
   def update
-    Document.update(params[:id], document_params)
+    @document = Document.find(params[:id])
+    @document.update(document_params)
+    @document.pdfs.create
     redirect_to documents_path
   end
   
@@ -36,5 +41,27 @@ class DocumentsController < ApplicationController
   private
   def document_params
     params.require(:document).permit(:name, :title, :content)
+  end
+
+  def request_pdf
+    queue = Rails.configuration.aws.pdfgen[:input_queue]
+    payload = {
+      generated_by: "newsify",
+      metadata_version: "0.1",
+      callback_url: api_pdf_url(@document.latest_pdf)
+    }
+
+    puts "Sending #{payload} to #{queue}"
+
+    sqs = Aws::SQS::Client.new(
+      region: Rails.configuration.aws.aws_region
+    )
+    resp = sqs.send_message(
+      queue_url: queue,
+      message_body: payload.to_json,
+      delay_seconds: 0
+    )
+
+    puts resp
   end
 end

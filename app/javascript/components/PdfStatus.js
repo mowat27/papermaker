70db9765942478b7c706c864825efb330ef62b3f
Adrian Mowat <adrian.mowat@gmail.com>
2021-01-03 20:37:18 +0000

    Use react to show different statuses

--- app/javascript/components/PdfStatus.js
+++ app/javascript/components/PdfStatus.js
@@ -1,7 +1,17 @@
-import React, { Component } from 'react'
+import React, {useState} from "react"
 
-const PdfStatus = () => (
-  <span className="pdf-download pending">TODO</span>
-)
+function PdfStatus (props) {
+  const [state, setState] = useState( () => {
+    return {
+      status: props.pdf ? props.pdf.status : "missing"
+    }
+  })
+
+  return (
+    <React.Fragment>
+      <span className={`pdf-status ${state.status}`}>{state.status}</span>
+    </React.Fragment>
+  )
+}
 
 export default PdfStatus
